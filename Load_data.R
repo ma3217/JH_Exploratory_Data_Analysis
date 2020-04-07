83c2fe7897719b2a6d5407b726a6c10d7fdea937
ma3217 <47382563+ma3217@users.noreply.github.com>
2020-04-07 20:50:08 +0100

    Update Load_data.R

--- Load_data.R
+++ Load_data.R
@@ -1,3 +1,10 @@
 
 
-DF <- read.table("/Users/user/Downloads/household_power_consumption.txt", header= TRUE)
+DF <- read.table("/Users/user/Downloads/household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+GAPower <- as.numeric(DF$Global_active_power)
+GRPower <- as.numeric(DF$Global_reactive_power)
+Vol <- as.numeric(DF$Voltage)
+SB1 <- as.numeric(DF$Sub_metering_1)
+SB2 <- as.numeric(DF$Sub_metering_2)
+SB3 <- as.numeric(DF$Sub_metering_3)
